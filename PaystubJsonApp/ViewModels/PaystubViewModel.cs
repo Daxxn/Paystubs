a390204c6e562f49faa82313f82d318d9e3785a5
Daxxn <nynjalantz@gmail.com>
2020-08-17 23:12:39 -0700

    Create Files

---
+++ PaystubJsonApp/ViewModels/PaystubViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaystubJsonApp.ViewModels
+{
+    public class PaystubViewModel : ViewModelBase
+    {
+        #region - Fields & Properties
+
+        #endregion
+
+        #region - Constructors
+        public PaystubViewModel( ) { }
+        #endregion
+
+        #region - Methods
+
+        #endregion
+
+        #region - Full Properties
+
+        #endregion
+    }
+}
---
+++ PaystubJsonApp/ViewModels/RepairOrderViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaystubJsonApp.ViewModels
+{
+    public class RepairOrderViewModel : ViewModelBase
+    {
+        #region - Fields & Properties
+
+        #endregion
+
+        #region - Constructors
+        public RepairOrderViewModel( ) { }
+        #endregion
+
+        #region - Methods
+
+        #endregion
+
+        #region - Full Properties
+
+        #endregion
+    }
+}
