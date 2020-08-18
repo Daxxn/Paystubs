72c63dbb584bfa401e1b3e62120542a760eb4658
Daxxn <nynjalantz@gmail.com>
2020-08-17 21:47:38 -0700

    Move Windows & Views to Views Folder

--- PaystubJsonApp/App.xaml.cs
+++ PaystubJsonApp/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using PaystubJsonApp.ViewModels;
+using PaystubJsonApp.Views;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
--- PaystubJsonApp/Views/MainWindow.xaml.cs
+++ PaystubJsonApp/Views/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PaystubJsonApp
+namespace PaystubJsonApp.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
